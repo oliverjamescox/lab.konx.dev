0843f66cfbd7b8949e7afc27f0fab695e26de72b
Oliver Cox <olivercoxdesign@gmail.com>
2020-12-16 01:21:33 +0000

    Create lighthouserc.js

---
+++ lighthouserc.js
@@ -0,0 +1,21 @@
+module.exports = {
+    "ci": {
+      "collect": {
+        "numberOfRuns": 3
+      },
+      "assert": {
+        "preset": "lighthouse:recommended",
+        "assertions": {
+          "first-contentful-paint": [
+            "error",
+            {"maxNumericValue": 2000, "aggregationMethod": "optimistic"}
+          ],
+          "interactive": ["error", {"maxNumericValue": 5000, "aggregationMethod": "optimistic"}]
+        }
+      },
+      "upload": {
+        "target": "lhci",
+        "serverBaseUrl": "https://lighthouse.konx.dev"
+      }
+    }
+}
