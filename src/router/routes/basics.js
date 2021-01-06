329f2890b5858fdb5df29cd60fd9760062b173eb
Oliver Cox <olivercoxdesign@gmail.com>
2021-01-06 00:53:31 +0000

    altered relative paths

--- src/router/routes/basics.js
+++ src/router/routes/basics.js
@@ -1,6 +1,6 @@
-const State = () => import('../../views/sets/basics/State.vue');
-const Binding = () => import('../../views/sets/basics/Binding.vue');
-const Events = () => import('../../views/sets/basics/Events.vue');
+const State = () => import('@/views/sets/basics/State.vue');
+const Binding = () => import('@/views/sets/basics/Binding.vue');
+const Events = () => import('@/views/sets/basics/Events.vue');
 
 export default [
     { path: '/vue/basics/state', component: State },
