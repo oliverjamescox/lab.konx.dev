01de6d719b9aa3c9bd0a9408568d1f49b1129126
Oliver Cox <olivercoxdesign@gmail.com>
2021-01-06 01:52:54 +0000

    changed arrow function to regular import

--- src/router/routes/basics.js
+++ src/router/routes/basics.js
@@ -1,9 +1,11 @@
-const State = () => import('@/views/sets/basics/State.vue');
-const Binding = () => import('@/views/sets/basics/Binding.vue');
-const Events = () => import('@/views/sets/basics/Events.vue');
+// const State = () => import('@/views/sets/basics/State.vue');
+// const Binding = () => import('@/views/sets/basics/Binding.vue');
+// const Events = () => import('@/views/sets/basics/Events.vue');
+
+import State from '../../views/sets/basics/State.vue';
 
 export default [
     { path: '/vue/basics/state', component: State },
-    { path: '/vue/basics/binding', component: Binding },
-    { path: '/vue/basics/events', component: Events },
+    // { path: '/vue/basics/binding', component: Binding },
+    // { path: '/vue/basics/events', component: Events },
 ];
--- src/router/routes/vue3-features.js
+++ src/router/routes/vue3-features.js
@@ -1,4 +1,6 @@
-const Overview = () => import('@/views/sets/vue3-features/Overview.vue');
+// const Overview = () => import('@/views/sets/vue3-features/Overview.vue');
+
+import Overview from '../../views/sets/vue3-features/Overview.vue';
 
 export default [
     { path: '/vue/vue3-features/overview', component: Overview }
