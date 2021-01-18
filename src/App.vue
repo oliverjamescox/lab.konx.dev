7520f8a384df8910a26fa570102170f5dc20c03f
Oliver Cox <olivercoxdesign@gmail.com>
2021-01-18 01:12:41 +0000

    Added compression library to serve gzip (#6)
    * updated readme
    
    * various
    
    * page finished
    
    * binding page
    
    * added plugins block and more content to basics
    
    * added example to state page and started on events page
    
    * binding & event page updates
    
    * reworked the nav
    
    * added holding page
    
    * added highlight.js support and swapped out all code blocks
    
    * vuex page added
    
    * vuex practical examples
    
    * added mutation practical example and continued to add to the vuex page
    
    * continued work on vuex page
    
    * body of vuex page completed, highlighted outstanding remaining sections
    
    * nav now closes on router view change
    
    * vuex page finished and added page header
    
    * added overflows to body & nav and styling for active router links
    
    * fixed overflow nav bug and updated existing pages with new heading component
    
    * work on homepage & new routes added
    
    * nav work across home & top level pages
    
    * refactored cards to use json and updated readme.md
    
    * updated routes
    
    * linted & added modern flag
    
    * added needs to chain workflow jobs
    
    * SEO fixes
    
    * git bugged and only commited assets
    
    * moved font import to head & added server caching
    
    * added compression library
    
    * triggering another lighthouse build
    
    * improved font loading
    
    * checking brotli server support
    
    * re-added lighthouse action to test brotli perf
    
    * analytics and web-vitals packages added
    
    * font revert & disabled analytics and web-vitals tracking

--- src/App.vue
+++ src/App.vue
@@ -9,19 +9,57 @@
         <router-view />
       </div>
       <!-- <holding-page></holding-page> -->
+      <!-- <button @click="gaTest()">ga test</button> -->
     </div>
   </div>
 </template>
 
 <script>
+import { reactive } from 'vue'
 // import HoldingPage from './components/Globals/HoldingPage.vue';
 import Navigation from "./components/Globals/Navigation.vue";
+
+// import {getCLS, getFID, getLCP} from 'web-vitals';
+// import { useGtag } from "vue-gtag-next";
+
 export default {
-  components: {
+  
+  setup() {
+
+    const data = reactive({
+    })
+
+    // const { event } = useGtag()
+
+    // function sendToGoogleAnalytics({name, delta, id}) {
+    //   event('event', name, {
+    //     event_category: 'Web Vitals',
+    //     value: Math.round(name === 'CLS' ? delta * 1000 : delta),
+    //     event_label: id,
+    //     non_interaction: true,
+    //   });
+    // }
+
+    // function gaTest() {
+    //   getCLS(sendToGoogleAnalytics);
+    //   getFID(sendToGoogleAnalytics);
+    //   getLCP(sendToGoogleAnalytics);
+    // }
+
+
+
+
+    return {
+      data,
+      // sendToGoogleAnalytics,
+      // gaTest
+    }
+  },
+    components: {
     "global-navigation": Navigation,
     // 'holding-page' : HoldingPage,
   }
-};
+}
 </script>
 
 <style lang="scss">
--- vue.config.js
+++ vue.config.js
@@ -11,6 +11,25 @@ module.exports = {
       .use('vue-svg-loader')
       .loader('vue-svg-loader');
   },
+  pluginOptions: {
+    compression:{
+      brotli: {
+        filename: '[path].br[query]',
+        algorithm: 'brotliCompress',
+        include: /\.(js|css|html|svg|json)(\?.*)?$/i,
+        compressionOptions: {
+          level: 11,
+        },
+        minRatio: 0.8,
+      }
+      // gzip: {
+      //   filename: '[path].gz[query]',
+      //   algorithm: 'gzip',
+      //   include: /\.(js|css|html|svg|json)(\?.*)?$/i,
+      //   minRatio: 0.8,
+      // }
+    }
+  },
   css: {
     loaderOptions: {
       sass: {
